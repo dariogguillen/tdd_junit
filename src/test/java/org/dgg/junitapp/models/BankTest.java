cf60e8215ba3394d4a95b5cc2e80a3f76a0f65f6
dariogguillen <guillendario@gmail.com>
2025-03-14 14:50:29 -0600

    fix: transfer implementation

---
+++ src/test/java/org/dgg/junitapp/models/BankTest.java
@@ -0,0 +1,12 @@
+package org.dgg.junitapp.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankTest {
+
+    @Test
+    void transfer() {
+    }
+}
