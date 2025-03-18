32c5f50787e90e7e3302ac43e5f7d1da485ec5b0
dariogguillen <guillendario@gmail.com>
2025-03-18 15:03:41 -0600

    Using before and after each

--- src/test/java/org/dgg/junitapp/models/AccountTest.java
+++ src/test/java/org/dgg/junitapp/models/AccountTest.java
@@ -1,30 +1,35 @@
 package org.dgg.junitapp.models;
 
 import org.dgg.junitapp.exceptions.NotEnoughMoneyException;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+    Account account;
+
+    @BeforeEach
+    void setUp() {
+        this.account = new Account("John Doe", new BigDecimal("1000.12345"));
+        System.out.println("Running test...");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Test completed.");
+    }
 
     @Test
     @DisplayName("Test account name")
-    @Disabled
     void testAccountName() {
-        fail();
-        Account account = new Account("John Doe", new BigDecimal("1000.12345"));
         assertEquals("John Doe", account.getPerson());
     }
 
     @Test
     @DisplayName("Test account balance")
-    @Deprecated
     void testAccountBalance() {
-        Account account = new Account("John Doe", new BigDecimal("1000.12345"));
         assertEquals(new BigDecimal("1000.12345"), account.getBalance());
         assertEquals(1000.12345, account.getBalance().doubleValue());
         assertFalse(new BigDecimal("1000.12345").compareTo(account.getBalance()) < 0);
@@ -34,7 +39,6 @@ class AccountTest {
     @Test
     @DisplayName("Test account reference")
     void accountReference() {
-        Account account = new Account("John Doe", new BigDecimal("1000.12345"));
         Account account2 = new Account("John Doe", new BigDecimal("1000.12345"));
 //        assertNotEquals(account2, account);
         assertEquals(account2, account);
@@ -42,7 +46,6 @@ class AccountTest {
 
     @Test
     void testDebit() {
-        Account account = new Account("John Doe", new BigDecimal("1000.12345"));
         account.debit(new BigDecimal("100"));
         assertNotNull(account.getBalance());
         assertEquals(new BigDecimal("900.12345"), account.getBalance());
@@ -51,7 +54,6 @@ class AccountTest {
 
     @Test
     void testCredit() {
-        Account account = new Account("John Doe", new BigDecimal("1000.12345"));
         account.credit(new BigDecimal("100"));
         assertNotNull(account.getBalance(), "Account can't be null");
         assertEquals(new BigDecimal("1100.12345"), account.getBalance(), "Account balance should be 1100.12345");
@@ -60,35 +62,32 @@ class AccountTest {
 
     @Test
     void testDebitNotEnoughMoney() {
-        Account account = new Account("John Doe", new BigDecimal("1000.12345"));
         Exception e = assertThrows(NotEnoughMoneyException.class, () -> account.debit(new BigDecimal("1000.12346")));
         assertEquals("Not enough money", e.getMessage());
     }
 
     @Test
     void testTransfer() {
-        Account from = new Account("John Doe", new BigDecimal("1000.12345"));
         Account to = new Account("Jane Doe", new BigDecimal("1000.12345"));
         Bank bank = new Bank("Bank of America");
-        bank.transfer(from, to, new BigDecimal("100"));
-        assertEquals(new BigDecimal("900.12345"), from.getBalance());
+        bank.transfer(account, to, new BigDecimal("100"));
+        assertEquals(new BigDecimal("900.12345"), account.getBalance());
         assertEquals(new BigDecimal("1100.12345"), to.getBalance());
     }
 
     @Test
     void testBankAccountRelation() {
-        Account from = new Account("John Doe", new BigDecimal("1000.12345"));
         Account to = new Account("Jane Doe", new BigDecimal("1000.12345"));
         Bank bank = new Bank("Bank of America");
-        bank.addAccount(from);
+        bank.addAccount(account);
         bank.addAccount(to);
-        bank.transfer(from, to, new BigDecimal("100"));
+        bank.transfer(account, to, new BigDecimal("100"));
         assertAll(
-                () -> assertEquals(new BigDecimal("900.12345"), from.getBalance()),
+                () -> assertEquals(new BigDecimal("900.12345"), account.getBalance()),
                 () -> assertEquals(new BigDecimal("1100.12345"), to.getBalance()),
-                () -> assertEquals(bank, from.getBank()),
+                () -> assertEquals(bank, account.getBank()),
                 () -> assertEquals(bank, to.getBank()),
-                () -> assertEquals("Bank of America", from.getBank().getName()),
+                () -> assertEquals("Bank of America", account.getBank().getName()),
                 () -> assertEquals(
                         "John Doe",
                         bank
